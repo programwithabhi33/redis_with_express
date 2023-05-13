01c4fe635ae8d1fcc5ec68fdaaa66b8d5d33b460
abhi <programwithabhi@gmail.com>
2023-05-13 23:16:36 +0530

    fetch data from the external api in the first call and store it in the redis database

--- index.js
+++ index.js
@@ -7,16 +7,26 @@ const Redis = require("redis");
 const client = Redis.createClient();
 client.connect()
 
-app.get('/', async (req, res) => {
-    let key = await client.get("name",(err,data)=>{
+app.get('/photos', async (req, res) => {
+
+    // find the key photos in the redis database using get function the first parameter is the key want to search and the second parameter is the callback function 
+    let key = await client.get("photos",(err,data)=>{
         return data;
     })
+
+    // if key is present in the database just return it 
     if(key != null) res.send(key)
-    else {
-        await client.set("name","abhishek")
-        let key = await client.get("name",(err,data)=> data)
-        res.send(key);
+    else{
+        // if not make http call and get it 
+        let resp = await fetch("https://jsonplaceholder.typicode.com/photos",{
+            method:"GET",
+        })
+        let res_json = await resp.json();
+        // and store in the database with key photos Note: store as string
+        await client.set("photos",JSON.stringify(res_json))
+        res.send(res_json);
     }
+
 })
 
 app.listen(port, () => {
